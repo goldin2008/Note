31505a2e0b2ecbbc71e8d575caab1d82a5b48678
goldin2008 <goldin20082011@gmail.com>
2022-06-21 23:02:34 -0400

    Update

--- coding/binarytree.py
+++ coding/binarytree.py
@@ -35,3 +35,51 @@ class Solution:
             if  root.right: helper(root.right, depth + 1)
         helper(root, 0)
         return res
+
+
+# 226.翻转二叉树
+## 递归法：前序遍历：
+class Solution:
+    def invertTree(self, root: TreeNode) -> TreeNode:
+        if not root:
+            return None
+        root.left, root.right = root.right, root.left #中
+        self.invertTree(root.left) #左
+        self.invertTree(root.right) #右
+        return root
+
+
+# 迭代法：深度优先遍历（前序遍历）：
+class Solution:
+    def invertTree(self, root: TreeNode) -> TreeNode:
+        if not root:
+            return root
+        st = []
+        st.append(root)
+        while st:
+            node = st.pop()
+            node.left, node.right = node.right, node.left #中
+            if node.right:
+                st.append(node.right) #右
+            if node.left:
+                st.append(node.left) #左
+        return root
+
+
+# 迭代法：广度优先遍历（层序遍历）：
+import collections
+class Solution:
+    def invertTree(self, root: TreeNode) -> TreeNode:
+        queue = collections.deque() #使用deque()
+        if root:
+            queue.append(root)
+        while queue:
+            size = len(queue)
+            for i in range(size):
+                node = queue.popleft()
+                node.left, node.right = node.right, node.left #节点处理
+                if node.left:
+                    queue.append(node.left)
+                if node.right:
+                    queue.append(node.right)
+        return root
