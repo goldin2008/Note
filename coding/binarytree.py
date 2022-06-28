174e5fe6ba1d867f002f3aa4bc0abc6dc01115d2
goldin2008 <goldin20082011@gmail.com>
2022-06-28 09:47:52 -0400

    Update

--- coding/binarytree.py
+++ coding/binarytree.py
@@ -83,3 +83,44 @@ class Solution:
                 if node.right:
                     queue.append(node.right)
         return root
+
+
+# 222.完全二叉树的节点个数
+# 递归法
+class Solution:
+    def countNodes(self, root: TreeNode) -> int:
+        return self.getNodesNum(root)
+        
+    def getNodesNum(self, cur):
+        if not cur:
+            return 0
+        leftNum = self.getNodesNum(cur.left) #左
+        rightNum = self.getNodesNum(cur.right) #右
+        treeNum = leftNum + rightNum + 1 #中
+        return treeNum
+
+# 递归法：精简版
+class Solution:
+    def countNodes(self, root: TreeNode) -> int:
+        if not root:
+            return 0
+        return 1 + self.countNodes(root.left) + self.countNodes(root.right)
+
+# 迭代法
+import collections
+class Solution:
+    def countNodes(self, root: TreeNode) -> int:
+        queue = collections.deque()
+        if root:
+            queue.append(root)
+        result = 0
+        while queue:
+            size = len(queue)
+            for i in range(size):
+                node = queue.popleft()
+                result += 1 #记录节点数量
+                if node.left:
+                    queue.append(node.left)
+                if node.right:
+                    queue.append(node.right)
+        return result
