b95ac95d4fae42a78803abc4fd9f7058b2c87453
goldin2008 <goldin20082011@gmail.com>
2022-08-05 20:30:21 -0400

    Update

--- coding/binarytree.py
+++ coding/binarytree.py
@@ -145,3 +145,20 @@ class Solution:
                 cur = cur.right
         return True
 
+
+# 236. 二叉树的最近公共祖先
+class Solution:
+    """二叉树的最近公共祖先 递归法"""
+
+    def lowestCommonAncestor(self, root: 'TreeNode', p: 'TreeNode', q: 'TreeNode') -> 'TreeNode':
+        if not root or root == p or root == q:
+            return root
+        
+        left = self.lowestCommonAncestor(root.left, p, q)
+        right = self.lowestCommonAncestor(root.right, p, q)
+        
+        if left and right:
+            return root
+        if left:
+            return left
+        return right
