6c5264f95aa0677362b1c8e462b324b6e11501cd
goldin2008 <goldin20082011@gmail.com>
2022-08-06 19:26:32 -0400

    Update

--- coding/binarytree.py
+++ coding/binarytree.py
@@ -259,3 +259,36 @@ class Solution:
         # 返回由被传入的左右界定义的某子树的根节点
         return mid_root
 
+
+# 538.把二叉搜索树转换为累加树
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def __init__(self):
+        self.pre = TreeNode()
+
+    def convertBST(self, root: Optional[TreeNode]) -> Optional[TreeNode]:
+        '''
+        倒序累加替换：  
+        [2, 5, 13] -> [[2]+[1]+[0], [2]+[1], [2]] -> [20, 18, 13]
+        '''
+        self.traversal(root)
+        return root
+
+    def traversal(self, root: TreeNode) -> None:
+        # 因为要遍历整棵树，所以递归函数不需要返回值
+        # Base Case
+        if not root: 
+            return None
+        # 单层递归逻辑：中序遍历的反译 - 右中左
+        self.traversal(root.right)  # 右
+
+        # 中节点：用当前root的值加上pre的值
+        root.val += self.pre.val    # 中
+        self.pre = root             
+
+        self.traversal(root.left)   # 左
