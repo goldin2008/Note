c5200553070513292b48afeb1b653cd71744bec5
goldin2008 <goldin20082011@gmail.com>
2022-08-07 18:15:34 -0400

    Update

--- coding/backtracking.py
+++ coding/backtracking.py
@@ -28,3 +28,38 @@ class Solution:
         backtrack(n,k,1)
         return res
 
+
+# 39. 组合总和
+class Solution:
+    def __init__(self):
+        self.path = []
+        self.paths = []
+
+    def combinationSum(self, candidates: List[int], target: int) -> List[List[int]]:
+        '''
+        因为本题没有组合数量限制，所以只要元素总和大于target就算结束
+        '''
+        self.path.clear()
+        self.paths.clear()
+
+        # 为了剪枝需要提前进行排序
+        candidates.sort()
+        self.backtracking(candidates, target, 0, 0)
+        return self.paths
+
+    def backtracking(self, candidates: List[int], target: int, sum_: int, start_index: int) -> None:
+        # Base Case
+        if sum_ == target:
+            self.paths.append(self.path[:]) # 因为是shallow copy，所以不能直接传入self.path
+            return
+        # 单层递归逻辑 
+        # 如果本层 sum + condidates[i] > target，就提前结束遍历，剪枝
+        for i in range(start_index, len(candidates)):
+            if sum_ + candidates[i] > target: 
+                return 
+            sum_ += candidates[i]
+            self.path.append(candidates[i])
+            self.backtracking(candidates, target, sum_, i)  # 因为无限制重复选取，所以不是i-1
+            sum_ -= candidates[i]   # 回溯
+            self.path.pop()        # 回溯
+
