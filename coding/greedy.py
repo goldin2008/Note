913536636a5fe925bb272956cf714f7c7df4258a
goldin2008 <goldin20082011@gmail.com>
2022-08-09 23:09:29 -0400

    Update

---
+++ coding/greedy.py
@@ -0,0 +1,27 @@
+# 455.分发饼干
+class Solution:
+    # 思路1：优先考虑胃饼干
+    def findContentChildren(self, g: List[int], s: List[int]) -> int:
+        g.sort()
+        s.sort()
+        res = 0
+        for i in range(len(s)):
+            if res <len(g) and s[i] >= g[res]:  #小饼干先喂饱小胃口
+                res += 1
+        return res
+
+
+# 53. 最大子序和
+class Solution:
+    def maxSubArray(self, nums: List[int]) -> int:
+        result = -float('inf')
+        count = 0
+        for i in range(len(nums)):
+            count += nums[i]
+            if count > result:
+                result = count
+            if count <= 0:
+                count = 0
+        return result
+
+
