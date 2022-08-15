6ec6e0c6ac967bacbbae90f8241986cf5c4e57d5
goldin2008 <goldin20082011@gmail.com>
2022-08-14 23:08:38 -0400

    Update

--- coding/greedy.py
+++ coding/greedy.py
@@ -65,3 +65,19 @@ class Solution:
         return ans
 
 
+
+# 134. 加油站
+class Solution:
+    def canCompleteCircuit(self, gas: List[int], cost: List[int]) -> int:
+        start = 0
+        curSum = 0
+        totalSum = 0
+        for i in range(len(gas)):
+            curSum += gas[i] - cost[i]
+            totalSum += gas[i] - cost[i]
+            if curSum < 0:
+                curSum = 0
+                start = i + 1
+        if totalSum < 0: return -1
+        return start
+
