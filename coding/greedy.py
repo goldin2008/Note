b6f872559e3a3bb55d968aee8f8b634abd1060ed
goldin2008 <goldin20082011@gmail.com>
2022-08-14 22:09:19 -0400

    Update

--- coding/greedy.py
+++ coding/greedy.py
@@ -48,3 +48,19 @@ class Solution:
         return False
 
 
+# 45.跳跃游戏II
+class Solution:
+    def jump(self, nums: List[int]) -> int:
+        if len(nums) == 1: return 0
+        ans = 0
+        curDistance = 0
+        nextDistance = 0
+        for i in range(len(nums)):
+            nextDistance = max(i + nums[i], nextDistance)
+            if i == curDistance: 
+                if curDistance != len(nums) - 1:
+                    ans += 1
+                    curDistance = nextDistance
+                    if nextDistance >= len(nums) - 1: break
+        return ans
+
