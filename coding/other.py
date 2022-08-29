593f79b35cc9eb742659c0d88d2a99b15ed45a8b
goldin2008 <goldin20082011@gmail.com>
2022-08-28 23:22:07 -0400

    Update

--- coding/other.py
+++ coding/other.py
@@ -35,4 +35,15 @@ class Solution:
                 stack.append(i)
         return result
 
+# 503.下一个更大元素II
+class Solution:
+    def nextGreaterElements(self, nums: List[int]) -> List[int]:
+        dp = [-1] * len(nums)
+        stack = []
+        for i in range(len(nums)*2):
+            while(len(stack) != 0 and nums[i%len(nums)] > nums[stack[-1]]):
+                    dp[stack[-1]] = nums[i%len(nums)]
+                    stack.pop()
+            stack.append(i%len(nums))
+        return dp
 
