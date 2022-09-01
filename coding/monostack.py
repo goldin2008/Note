71e1e61520c046244552d4e7e835da0e45c561a8
goldin2008 <goldin20082011@gmail.com>
2022-09-01 12:33:13 -0400

    Update

--- coding/monostack.py
+++ coding/monostack.py
@@ -1,3 +1,20 @@
+# 739. 每日温度
+class Solution:
+    def dailyTemperatures(self, temperatures: List[int]) -> List[int]:
+        answer = [0]*len(temperatures)
+        stack = [0]
+        for i in range(1,len(temperatures)):
+            # 情况一和情况二
+            if temperatures[i]<=temperatures[stack[-1]]:
+                stack.append(i)
+            # 情况三
+            else:
+                while len(stack) != 0 and temperatures[i]>temperatures[stack[-1]]:
+                    answer[stack[-1]]=i-stack[-1]
+                    stack.pop()
+                stack.append(i)
+            
+        return answer
 
 # 496.下一个更大元素 I
 class Solution:
