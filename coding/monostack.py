e32bb68161d9c4b661970e62793014d87c438a38
goldin2008 <goldin20082011@gmail.com>
2022-09-01 12:32:10 -0400

    Update

--- coding/monostack.py
+++ coding/monostack.py
@@ -1,3 +1,17 @@
+
+
+# 503.下一个更大元素II
+class Solution:
+    def nextGreaterElements(self, nums: List[int]) -> List[int]:
+        dp = [-1] * len(nums)
+        stack = []
+        for i in range(len(nums)*2):
+            while(len(stack) != 0 and nums[i%len(nums)] > nums[stack[-1]]):
+                    dp[stack[-1]] = nums[i%len(nums)]
+                    stack.pop()
+            stack.append(i%len(nums))
+        return dp
+
 # 42. 接雨水
 class Solution:
     def trap(self, height: List[int]) -> int:
