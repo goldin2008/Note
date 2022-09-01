a99e69f96726beee4e77c21fb1b2ff1e5da85b1a
goldin2008 <goldin20082011@gmail.com>
2022-09-01 12:32:40 -0400

    Update

--- coding/monostack.py
+++ coding/monostack.py
@@ -1,4 +1,22 @@
 
+# 496.下一个更大元素 I
+class Solution:
+    def nextGreaterElement(self, nums1: List[int], nums2: List[int]) -> List[int]:
+        result = [-1]*len(nums1)
+        stack = [0]
+        for i in range(1,len(nums2)):
+            # 情况一情况二
+            if nums2[i]<=nums2[stack[-1]]:
+                stack.append(i)
+            # 情况三
+            else:
+                while len(stack)!=0 and nums2[i]>nums2[stack[-1]]:
+                    if nums2[stack[-1]] in nums1:
+                        index = nums1.index(nums2[stack[-1]])
+                        result[index]=nums2[i]
+                    stack.pop()                 
+                stack.append(i)
+        return result
 
 # 503.下一个更大元素II
 class Solution:
