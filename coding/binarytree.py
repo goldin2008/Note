d553c8186d8d8e03f7eb1c8842610d831474953a
goldin2008 <goldin20082011@gmail.com>
2022-09-03 17:11:31 -0400

    Update

--- coding/binarytree.py
+++ coding/binarytree.py
@@ -28,8 +28,8 @@
 二叉树中深度优先和广度优先遍历实现方式，我们做二叉树相关题目，经常会使用递归的方式来实现深度优先遍历，也就是实现前中后序遍历，使用递归是比较方便的。
 栈其实就是递归的一种是实现结构，也就说前中后序遍历的逻辑其实都是可以借助栈使用递归的方式来实现的。
 广度优先遍历的实现一般使用队列来实现，这也是队列先进先出的特点所决定的，因为需要先进先出的结构，才能一层一层的来遍历二叉树。
-
 """
+
 # 二叉树节点定义
 class TreeNode: 
     def __init__(self, value):
@@ -38,7 +38,7 @@ class TreeNode:
         self.right = None
 
 """
-递归
+深度优先 递归
 """
 # 前序遍历-递归-LC144_二叉树的前序遍历
 class Solution:
@@ -79,67 +79,8 @@ class Solution:
         return result
 
 """
-迭代
+深度优先 迭代
 """
-# 前序遍历-迭代-LC144_二叉树的前序遍历
-class Solution:
-    def preorderTraversal(self, root: TreeNode) -> List[int]:
-        # 根结点为空则返回空列表
-        if not root:
-            return []
-        stack = [root]
-        result = []
-        while stack:
-            node = stack.pop()
-            # 中结点先处理
-            result.append(node.val)
-            # 右孩子先入栈
-            if node.right:
-                stack.append(node.right)
-            # 左孩子后入栈
-            if node.left:
-                stack.append(node.left)
-        return result
-# 中序遍历-迭代-LC94_二叉树的中序遍历
-class Solution:
-    def inorderTraversal(self, root: TreeNode) -> List[int]:
-        if not root:
-            return []
-        stack = []  # 不能提前将root结点加入stack中
-        result = []
-        cur = root
-        while cur or stack:
-            # 先迭代访问最底层的左子树结点
-            if cur:     
-                stack.append(cur)
-                cur = cur.left		
-            # 到达最左结点后处理栈顶结点    
-            else:		
-                cur = stack.pop()
-                result.append(cur.val)
-                # 取栈顶元素右结点
-                cur = cur.right	
-        return result
-# 后序遍历-迭代-LC145_二叉树的后序遍历
-class Solution:
-    def postorderTraversal(self, root: TreeNode) -> List[int]:
-        if not root:
-            return []
-        stack = [root]
-        result = []
-        while stack:
-            node = stack.pop()
-            # 中结点先处理
-            result.append(node.val)
-            # 左孩子先入栈
-            if node.left:
-                stack.append(node.left)
-            # 右孩子后入栈
-            if node.right:
-                stack.append(node.right)
-        # 将最终的数组翻转
-        return result[::-1]
-
 # 迭代统一写法
 # 迭代法前序遍历：
 class Solution:
@@ -205,7 +146,9 @@ class Solution:
                 result.append(node.val)
         return result
 
-#层序遍,历迭代解法
+"""
+广度优先(层序遍历) 迭代
+"""
 class Solution:
     """二叉树层序遍历迭代解法"""
     def levelOrder(self, root: TreeNode) -> List[List[int]]:
