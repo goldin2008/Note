c6b41f0eb7e6a9e42122c3c1cd4a6b37c8bfc774
goldin2008 <goldin20082011@gmail.com>
2022-09-03 15:07:44 -0400

    Update

--- coding/binarytree.py
+++ coding/binarytree.py
@@ -37,77 +37,108 @@ class TreeNode:
         self.left = None
         self.right = None
 
+"""
+递归
+"""
 # 前序遍历-递归-LC144_二叉树的前序遍历
 class Solution:
     def preorderTraversal(self, root: TreeNode) -> List[int]:
         # 保存结果
         result = []
-        
         def traversal(root: TreeNode):
             if root == None:
                 return
             result.append(root.val) # 前序
             traversal(root.left)    # 左
             traversal(root.right)   # 右
-
         traversal(root)
         return result
-
 # 中序遍历-递归-LC94_二叉树的中序遍历
 class Solution:
     def inorderTraversal(self, root: TreeNode) -> List[int]:
         result = []
-
         def traversal(root: TreeNode):
             if root == None:
                 return
             traversal(root.left)    # 左
             result.append(root.val) # 中序
             traversal(root.right)   # 右
-
         traversal(root)
         return result
-
 # 后序遍历-递归-LC145_二叉树的后序遍历
 class Solution:
     def postorderTraversal(self, root: TreeNode) -> List[int]:
         result = []
-
         def traversal(root: TreeNode):
             if root == None:
                 return
             traversal(root.left)    # 左
             traversal(root.right)   # 右
             result.append(root.val) # 后序
-
         traversal(root)
         return result
 
-# 二叉树层序遍历迭代解法
+"""
+迭代
+"""
+# 前序遍历-迭代-LC144_二叉树的前序遍历
 class Solution:
-    """二叉树层序遍历迭代解法"""
-
-    def levelOrder(self, root: TreeNode) -> List[List[int]]:
-        results = []
+    def preorderTraversal(self, root: TreeNode) -> List[int]:
+        # 根结点为空则返回空列表
         if not root:
-            return results
-        
-        from collections import deque
-        que = deque([root])
-        
-        while que:
-            size = len(que)
-            result = []
-            for _ in range(size):
-                cur = que.popleft()
+            return []
+        stack = [root]
+        result = []
+        while stack:
+            node = stack.pop()
+            # 中结点先处理
+            result.append(node.val)
+            # 右孩子先入栈
+            if node.right:
+                stack.append(node.right)
+            # 左孩子后入栈
+            if node.left:
+                stack.append(node.left)
+        return result
+# 中序遍历-迭代-LC94_二叉树的中序遍历
+class Solution:
+    def inorderTraversal(self, root: TreeNode) -> List[int]:
+        if not root:
+            return []
+        stack = []  # 不能提前将root结点加入stack中
+        result = []
+        cur = root
+        while cur or stack:
+            # 先迭代访问最底层的左子树结点
+            if cur:     
+                stack.append(cur)
+                cur = cur.left		
+            # 到达最左结点后处理栈顶结点    
+            else:		
+                cur = stack.pop()
                 result.append(cur.val)
-                if cur.left:
-                    que.append(cur.left)
-                if cur.right:
-                    que.append(cur.right)
-            results.append(result)
-
-        return results
+                # 取栈顶元素右结点
+                cur = cur.right	
+        return result
+# 后序遍历-迭代-LC145_二叉树的后序遍历
+class Solution:
+    def postorderTraversal(self, root: TreeNode) -> List[int]:
+        if not root:
+            return []
+        stack = [root]
+        result = []
+        while stack:
+            node = stack.pop()
+            # 中结点先处理
+            result.append(node.val)
+            # 左孩子先入栈
+            if node.left:
+                stack.append(node.left)
+            # 右孩子后入栈
+            if node.right:
+                stack.append(node.right)
+        # 将最终的数组翻转
+        return result[::-1]
 
 # 递归法
 class Solution:
@@ -121,6 +152,31 @@ class Solution:
             if  root.right: helper(root.right, depth + 1)
         helper(root, 0)
         return res
+
+# 二叉树层序遍历迭代解法
+class Solution:
+    """二叉树层序遍历迭代解法"""
+    def levelOrder(self, root: TreeNode) -> List[List[int]]:
+        results = []
+        if not root:
+            return results
+
+        from collections import deque
+        que = deque([root])
+
+        while que:
+            size = len(que)
+            result = []
+            for _ in range(size):
+                cur = que.popleft()
+                result.append(cur.val)
+                if cur.left:
+                    que.append(cur.left)
+                if cur.right:
+                    que.append(cur.right)
+            results.append(result)
+
+        return results
 
 
 # 226.翻转二叉树
