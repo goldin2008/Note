b1285dfcf1f98d7107af1def341ffbf99e4abefa
goldin2008 <goldin20082011@gmail.com>
2022-09-08 22:13:40 -0400

    Update

--- coding/backtracking.py
+++ coding/backtracking.py
@@ -33,12 +33,41 @@ class Solution:
             if len(path) == k:
                 res.append(path[:])
                 return
+            # for i in range(startIndex, n+1):
             for i in range(startIndex,n-(k-len(path))+2):  #优化的地方
                 path.append(i)  #处理节点
                 backtrack(n,k,i+1)  #递归
                 path.pop()  #回溯，撤销处理的节点
         backtrack(n,k,1)
         return res
+
+# 216.组合总和III
+class Solution:
+    def __init__(self):
+        self.res = []
+        self.sum_now = 0
+        self.path = []
+
+    def combinationSum3(self, k: int, n: int) -> List[List[int]]:
+        self.backtracking(k, n, 1)
+        return self.res
+
+    def backtracking(self, k: int, n: int, start_num: int):
+        if self.sum_now > n:  # 剪枝
+            return
+        if len(self.path) == k:  # len(path)==k时不管sum是否等于n都会返回
+            if self.sum_now == n:
+                self.res.append(self.path[:])
+            return
+        for i in range(start_num, 10 - (k - len(self.path)) + 1):
+            self.path.append(i)
+            self.sum_now += i
+            self.backtracking(k, n, i + 1)
+            self.path.pop()
+            self.sum_now -= i
+
+
+# 17.电话号码的字母组合
 
 
 # 39. 组合总和
