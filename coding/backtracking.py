ba00423a09b6f3042ae8ed62c0b0c5eab72f4380
goldin2008 <goldin20082011@gmail.com>
2022-09-10 22:29:33 -0400

    Update

--- coding/backtracking.py
+++ coding/backtracking.py
@@ -435,3 +435,81 @@ class Solution:
         return res
 
 
+# 491.递增子序列
+# 给定一个整型数组, 你的任务是找到所有该数组的递增子序列，递增子序列的长度至少是2。
+# 输入: [4, 6, 7, 7]
+# 输出: [[4, 6], [4, 7], [4, 6, 7], [4, 6, 7, 7], [6, 7], [6, 7, 7], [7,7], [4,7,7]]
+# 本题求子序列，很明显一个元素不能重复使用，所以需要startIndex，调整下一层递归的起始位置。
+# 回溯
+class Solution:
+    def __init__(self):
+        self.paths = []
+        self.path = []
+
+    def findSubsequences(self, nums: List[int]) -> List[List[int]]:
+        '''
+        本题求自增子序列，所以不能改变原数组顺序
+        '''
+        self.backtracking(nums, 0)
+        return self.paths
+
+    def backtracking(self, nums: List[int], start_index: int):
+        # 收集结果，同78.子集，仍要置于终止条件之前
+        if len(self.path) >= 2:
+            # 本题要求所有的节点
+            self.paths.append(self.path[:])
+        
+        # Base Case（可忽略）
+        if start_index == len(nums):
+            return
+
+        # 单层递归逻辑
+        # 深度遍历中每一层都会有一个全新的usage_list用于记录本层元素是否重复使用
+        usage_list = set()
+        # 同层横向遍历
+        for i in range(start_index, len(nums)):
+            # 若当前元素值小于前一个时（非递增）或者曾用过，跳入下一循环
+            if (self.path and nums[i] < self.path[-1]) or nums[i] in usage_list:
+                continue
+            usage_list.add(nums[i])
+            self.path.append(nums[i])
+            self.backtracking(nums, i+1)
+            self.path.pop() 
+
+# 回溯+哈希表去重
+class Solution:
+    def __init__(self):
+        self.paths = []
+        self.path = []
+
+    def findSubsequences(self, nums: List[int]) -> List[List[int]]:
+        '''
+        本题求自增子序列，所以不能改变原数组顺序
+        '''
+        self.backtracking(nums, 0)
+        return self.paths
+
+    def backtracking(self, nums: List[int], start_index: int):
+        # 收集结果，同78.子集，仍要置于终止条件之前
+        if len(self.path) >= 2:
+            # 本题要求所有的节点
+            self.paths.append(self.path[:])
+        
+        # Base Case（可忽略）
+        if start_index == len(nums):
+            return
+
+        # 单层递归逻辑
+        # 深度遍历中每一层都会有一个全新的usage_list用于记录本层元素是否重复使用
+        usage_list = [False] * 201  # 使用列表去重，题中取值范围[-100, 100]
+        # 同层横向遍历
+        for i in range(start_index, len(nums)):
+            # 若当前元素值小于前一个时（非递增）或者曾用过，跳入下一循环
+            if (self.path and nums[i] < self.path[-1]) or usage_list[nums[i]+100] == True:
+                continue
+            usage_list[nums[i]+100] = True
+            self.path.append(nums[i])
+            self.backtracking(nums, i+1)
+            self.path.pop()
+
+
