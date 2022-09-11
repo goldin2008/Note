3d134f6cf7d923414f0f3edb1d3083d3eacb900a
goldin2008 <goldin20082011@gmail.com>
2022-09-10 21:24:59 -0400

    Update

--- coding/backtracking.py
+++ coding/backtracking.py
@@ -49,6 +49,7 @@ class Solution:
         backtrack(n,k,1)
         return res
 
+
 # 216.组合总和III
 # 找出所有相加之和为 n 的 k 个数的组合。组合中只允许含有 1 - 9 的正整数，并且每种组合中不存在重复的数字。
 class Solution:
@@ -182,6 +183,7 @@ class Solution:
             sum_ -= candidates[i]   # 回溯
             self.path.pop()        # 回溯
 
+
 # 40.组合总和II
 # 给定一个数组 candidates 和一个目标数 target ，找出 candidates 中所有可以使数字和为 target 的组合。
 # 本题的难点在于区别2中：集合（数组candidates）有重复元素，但还不能有重复的组合。
@@ -223,5 +225,88 @@ class Solution:
             self.path.pop()             # 回溯，为了下一轮for loop
             sum_ -= candidates[i]       # 回溯，为了下一轮for loop
 
+
 # 131.分割回文串
 # 给定一个字符串 s，将 s 分割成一些子串，使每个子串都是回文串。
+# 回溯+正反序判断回文串
+class Solution:
+    def __init__(self):
+        self.paths = []
+        self.path = []
+
+    def partition(self, s: str) -> List[List[str]]:
+        '''
+        递归用于纵向遍历
+        for循环用于横向遍历
+        当切割线迭代至字符串末尾，说明找到一种方法
+        类似组合问题，为了不重复切割同一位置，需要start_index来做标记下一轮递归的起始位置(切割线)
+        '''
+        self.path.clear()
+        self.paths.clear()
+        self.backtracking(s, 0)
+        return self.paths
+
+    def backtracking(self, s: str, start_index: int) -> None:
+        # Base Case
+        if start_index >= len(s):
+            self.paths.append(self.path[:])
+            return
+        
+        # 单层递归逻辑
+        for i in range(start_index, len(s)):
+            # 此次比其他组合题目多了一步判断：
+            # 判断被截取的这一段子串([start_index, i])是否为回文串
+            temp = s[start_index:i+1]
+            if temp == temp[::-1]:  # 若反序和正序相同，意味着这是回文串
+                self.path.append(temp)
+                self.backtracking(s, i+1)   # 递归纵向遍历：从下一处进行切割，判断其余是否仍为回文串
+                self.path.pop()
+            else:
+                continue
+
+# 回溯+函数判断回文串
+class Solution:
+    def __init__(self):
+        self.paths = []
+        self.path = []
+
+    def partition(self, s: str) -> List[List[str]]:
+        '''
+        递归用于纵向遍历
+        for循环用于横向遍历
+        当切割线迭代至字符串末尾，说明找到一种方法
+        类似组合问题，为了不重复切割同一位置，需要start_index来做标记下一轮递归的起始位置(切割线)
+        '''
+        self.path.clear()
+        self.paths.clear()
+        self.backtracking(s, 0)
+        return self.paths
+
+    def backtracking(self, s: str, start_index: int) -> None:
+        # Base Case
+        if start_index >= len(s):
+            self.paths.append(self.path[:])
+            return
+        
+        # 单层递归逻辑
+        for i in range(start_index, len(s)):
+            # 此次比其他组合题目多了一步判断：
+            # 判断被截取的这一段子串([start_index, i])是否为回文串
+            if self.is_palindrome(s, start_index, i):
+                self.path.append(s[start_index:i+1])
+                self.backtracking(s, i+1)   # 递归纵向遍历：从下一处进行切割，判断其余是否仍为回文串
+                self.path.pop()             # 回溯
+            else:
+                continue    
+
+    def is_palindrome(self, s: str, start: int, end: int) -> bool:
+        i: int = start        
+        j: int = end
+        while i < j:
+            if s[i] != s[j]:
+                return False
+            i += 1
+            j -= 1
+        return True
+
+
