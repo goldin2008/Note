c771e1e19eb6920f462af4e04b2883c9678ba7c2
goldin2008 <goldin20082011@gmail.com>
2022-09-12 08:43:37 -0400

    Update

--- coding/dynamic.py
+++ coding/dynamic.py
@@ -7,3 +7,28 @@
 
 
 """
+#1 509. 斐波那契数
+# 斐波那契数，通常用 F(n) 表示，形成的序列称为 斐波那契数列 。该数列由 0 和 1 开始，后面的每一项数字都是前面两项数字的和。也就是： F(0) = 0，F(1) = 1 F(n) = F(n - 1) + F(n - 2)，其中 n > 1 给你n ，请计算 F(n) 。
+class Solution:
+    def fib(self, n: int) -> int:
+        if n < 2:
+            return n
+        a, b, c = 0, 1, 0
+        for i in range(1, n):
+            c = a + b
+            a, b = b, c
+        return c
+
+# 递归实现
+class Solution:
+    def fib(self, n: int) -> int:
+        if n < 2:
+            return n
+        return self.fib(n - 1) + self.fib(n - 2)
+
+
+#2 70. 爬楼梯
+# 假设你正在爬楼梯。需要 n 阶你才能到达楼顶。
+# 每次你可以爬 1 或 2 个台阶。你有多少种不同的方法可以爬到楼顶呢？
+# 注意：给定 n 是一个正整数。
+
