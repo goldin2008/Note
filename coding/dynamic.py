53915886e7fc38b54082d963f1d1548e3e10e079
goldin2008 <goldin20082011@gmail.com>
2022-09-12 22:50:57 -0400

    Update

--- coding/dynamic.py
+++ coding/dynamic.py
@@ -67,3 +67,16 @@ class Solution:
         return min(dp[len(cost) - 1], dp[len(cost) - 2])
 
 
+#4 62.不同路径
+# 一个机器人位于一个 m x n 网格的左上角 （起始点在下图中标记为 “Start” ）。
+# 机器人每次只能向下或者向右移动一步。机器人试图达到网格的右下角（在下图中标记为 “Finish” ）。
+# 问总共有多少条不同的路径？
+class Solution: # 动态规划
+    def uniquePaths(self, m: int, n: int) -> int:
+        dp = [[1 for i in range(n)] for j in range(m)]
+        for i in range(1, m):
+            for j in range(1, n):
+                dp[i][j] = dp[i][j - 1] + dp[i - 1][j]
+        return dp[m - 1][n - 1]
+
+
