d9baf3070eb1f2680292b58e578736d1a69048df
goldin2008 <goldin20082011@gmail.com>
2022-09-12 22:32:49 -0400

    Update

--- coding/dynamic.py
+++ coding/dynamic.py
@@ -4,8 +4,6 @@
 3. dp数组如何初始化
 4. 确定遍历顺序
 5. 举例推导dp数组
-
-
 """
 #1 509. 斐波那契数
 # 斐波那契数，通常用 F(n) 表示，形成的序列称为 斐波那契数列 。该数列由 0 和 1 开始，后面的每一项数字都是前面两项数字的和。也就是： F(0) = 0，F(1) = 1 F(n) = F(n - 1) + F(n - 2)，其中 n > 1 给你n ，请计算 F(n) 。
@@ -18,7 +16,6 @@ class Solution:
             c = a + b
             a, b = b, c
         return c
-
 # 递归实现
 class Solution:
     def fib(self, n: int) -> int:
@@ -31,4 +28,42 @@ class Solution:
 # 假设你正在爬楼梯。需要 n 阶你才能到达楼顶。
 # 每次你可以爬 1 或 2 个台阶。你有多少种不同的方法可以爬到楼顶呢？
 # 注意：给定 n 是一个正整数。
+# 空间复杂度为O(n)版本
+class Solution:
+    def climbStairs(self, n: int) -> int:
+        # dp[i] 为第 i 阶楼梯有多少种方法爬到楼顶
+        dp=[0]*(n+1)
+        dp[0]=1
+        dp[1]=1
+        for i in range(2,n+1):
+            dp[i]=dp[i-1]+dp[i-2]
+        return dp[n]
+# 空间复杂度为O(1)版本
+class Solution:
+    def climbStairs(self, n: int) -> int:
+        dp=[0]*(n+1)
+        dp[0]=1
+        dp[1]=1
+        for i in range(2,n+1):
+            tmp=dp[0]+dp[1]
+            dp[0]=dp[1]
+            dp[1]=tmp
+        return dp[1]
+
+
+#3 ??? 746. 使用最小花费爬楼梯
+# 数组的每个下标作为一个阶梯，第 i 个阶梯对应着一个非负数的体力花费值 cost[i]（下标从 0 开始）。
+# 每当你爬上一个阶梯你都要花费对应的体力值，一旦支付了相应的体力值，你就可以选择向上爬一个阶梯或者爬两个阶梯。
+# 请你找出达到楼层顶部的最低花费。在开始时，你可以选择从下标为 0 或 1 的元素作为初始阶梯。
+# 示例 1：输入：cost = [10, 15, 20] 输出：15 解释：最低花费是从 cost[1] 开始，然后走两步即可到阶梯顶，一共花费 15 。
+# 示例 2：输入：cost = [1, 100, 1, 1, 1, 100, 1, 1, 100, 1] 输出：6 解释：最低花费方式是从 cost[0] 开始，逐个经过那些 1 ，跳过 cost[3] ，一共花费 6 。
+class Solution:
+    def minCostClimbingStairs(self, cost: List[int]) -> int:
+        dp = [0] * (len(cost))
+        dp[0] = cost[0]
+        dp[1] = cost[1]
+        for i in range(2, len(cost)):
+            dp[i] = min(dp[i - 1], dp[i - 2]) + cost[i]
+        return min(dp[len(cost) - 1], dp[len(cost) - 2])
+
 
