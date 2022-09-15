ad52f676442fe695f38976d34d93527ea1ea0cb3
goldin2008 <goldin20082011@gmail.com>
2022-09-14 22:09:22 -0400

    Update

--- coding/dynamic.py
+++ coding/dynamic.py
@@ -146,3 +146,16 @@ class Solution:
         return dp[n]
 
 
+#7 ??? 96.不同的二叉搜索树
+# 给定一个整数 n，求以 1 ... n 为节点组成的二叉搜索树有多少种？
+# dp[i] += dp[j - 1] * dp[i - j]
+class Solution:
+    def numTrees(self, n: int) -> int:
+        dp = [0] * (n + 1)
+        dp[0], dp[1] = 1, 1
+        for i in range(2, n + 1):
+            for j in range(1, i + 1):
+                dp[i] += dp[j - 1] * dp[i - j]
+        return dp[-1]
+
+
