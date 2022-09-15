4a91dd716b6df89b7766d699356ebba4d0c7e555
goldin2008 <goldin20082011@gmail.com>
2022-09-14 22:07:10 -0400

    Update

--- coding/dynamic.py
+++ coding/dynamic.py
@@ -121,7 +121,7 @@ class Solution:
         return dp[-1][-1]
 
 
-#6 343. 整数拆分
+#6 ??? 343. 整数拆分
 # 给定一个正整数 n，将其拆分为至少两个正整数的和，并使这些整数的乘积最大化。 返回你可以获得的最大乘积。
 # 示例 1:
 # 输入: 2
@@ -132,3 +132,17 @@ class Solution:
 # 输出: 36
 # 解释: 10 = 3 + 3 + 4, 3 × 3 × 4 = 36。
 # 说明: 你可以假设 n 不小于 2 且不大于 58
+# dp[i] = max(dp[i], dp[i - j] * dp[j])
+class Solution:
+    def integerBreak(self, n: int) -> int:
+        dp = [0] * (n + 1)
+        dp[2] = 1
+        for i in range(3, n + 1):
+            # 假设对正整数 i 拆分出的第一个正整数是 j（1 <= j < i），则有以下两种方案：
+            # 1) 将 i 拆分成 j 和 i−j 的和，且 i−j 不再拆分成多个正整数，此时的乘积是 j * (i-j)
+            # 2) 将 i 拆分成 j 和 i−j 的和，且 i−j 继续拆分成多个正整数，此时的乘积是 j * dp[i-j]
+            for j in range(1, i - 1):
+                dp[i] = max(dp[i], max(j * (i - j), j * dp[i - j]))
+        return dp[n]
+
+
