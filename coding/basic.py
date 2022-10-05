e7007cc05f34147386c9453b54dd1face956c66a
goldin2008 <goldin20082011@gmail.com>
2022-10-04 23:32:36 -0400

    Update

--- coding/basic.py
+++ coding/basic.py
@@ -2,10 +2,12 @@
 Sorting
 https://realpython.com/sorting-algorithms-python/
 """
-# Bubble Sort
-# Time complexity O(n2)
-# Space complexity O(n)
 
+"""
+Bubble Sort
+Time complexity O(n2)
+Space complexity O(n)
+"""
 def bubble_sort(array):
     n = len(array)
 
@@ -37,4 +39,39 @@ def bubble_sort(array):
 
     return array
 
+"""
+Insertion Sort
+The insertion sort algorithm works exactly like the example with the deck of cards.
+Time complexity O(n2)
+Space complexity O(n)
+"""
+def insertion_sort(array):
+    # Loop from the second element of the array until
+    # the last element
+    for i in range(1, len(array)):
+        # This is the element we want to position in its
+        # correct place
+        key_item = array[i]
+
+        # Initialize the variable that will be used to
+        # find the correct position of the element referenced
+        # by `key_item`
+        j = i - 1
+
+        # Run through the list of items (the left
+        # portion of the array) and find the correct position
+        # of the element referenced by `key_item`. Do this only
+        # if `key_item` is smaller than its adjacent values.
+        while j >= 0 and array[j] > key_item:
+            # Shift the value one position to the left
+            # and reposition j to point to the next element
+            # (from right to left)
+            array[j + 1] = array[j]
+            j -= 1
+
+        # When you finish shifting the elements, you can position
+        # `key_item` in its correct location
+        array[j + 1] = key_item
+
+    return array
 
