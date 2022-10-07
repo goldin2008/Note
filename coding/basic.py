0bf7b9a14de1fcc5b7a11d5de19534eb93860c7b
goldin2008 <goldin20082011@gmail.com>
2022-10-07 15:23:17 -0400

    Update

--- coding/basic.py
+++ coding/basic.py
@@ -397,7 +397,7 @@ Counting Sort
 # Python program for counting sort
 # The main function that sort the given string arr[] in 
 # alphabetical order
-def countSort(arr):
+def count_sort(arr):
   
     # The output character array that will have sorted arr
     output = [0 for i in range(len(arr))]
@@ -429,6 +429,43 @@ def countSort(arr):
     for i in range(len(arr)):
         ans[i] = output[i]
     return ans
+
+"""
+Bucket Sort
+"""
+def insertionSort(b):
+    for i in range(1, len(b)):
+        up = b[i]
+        j = i - 1
+        while j >= 0 and b[j] > up: 
+            b[j + 1] = b[j]
+            j -= 1
+        b[j + 1] = up     
+    return b     
+              
+def bucket_sort(x):
+    arr = []
+    slot_num = 10 # 10 means 10 slots, each
+                  # slot's size is 0.1
+    for i in range(slot_num):
+        arr.append([])
+          
+    # Put array elements in different buckets 
+    for j in x:
+        index_b = int(slot_num * j) 
+        arr[index_b].append(j)
+      
+    # Sort individual buckets 
+    for i in range(slot_num):
+        arr[i] = insertionSort(arr[i])
+          
+    # concatenate the result
+    k = 0
+    for i in range(slot_num):
+        for j in range(len(arr[i])):
+            x[k] = arr[i][j]
+            k += 1
+    return x
 
 
 ARRAY_LENGTH = 10000
@@ -468,3 +505,9 @@ if __name__ == "__main__":
 
     run_sorting_algorithm(algorithm="radix_sort", array=array)
 # Algorithm: radix_sort. Minimum execution time: 0.39657199999999193
+
+    run_sorting_algorithm(algorithm="count_sort", array=array)
+# Algorithm: count_sort. Minimum execution time: 0.39657199999999193
+
+    run_sorting_algorithm(algorithm="bucket_sort", array=array)
+# Algorithm: bucket_sort. Minimum execution time: 0.39657199999999193
