1e68a1805a9ccacd5d6dc7a320f47c273ead8503
goldin2008 <goldin20082011@gmail.com>
2022-10-07 00:14:51 -0400

    Update

--- coding/basic.py
+++ coding/basic.py
@@ -1,3 +1,23 @@
+from random import randint
+from timeit import repeat
+
+def run_sorting_algorithm(algorithm, array):
+    # Set up the context and prepare the call to the specified
+    # algorithm using the supplied array. Only import the
+    # algorithm function if it's not the built-in `sorted()`.
+    setup_code = f"from __main__ import {algorithm}" \
+        if algorithm != "sorted" else ""
+
+    stmt = f"{algorithm}({array})"
+
+    # Execute the code ten different times and return the time
+    # in seconds that each execution took
+    times = repeat(setup=setup_code, stmt=stmt, repeat=3, number=10)
+
+    # Finally, display the name of the algorithm and the
+    # minimum time it took to run
+    print(f"Algorithm: {algorithm}. Minimum execution time: {min(times)}")
+
 """
 Sorting
 https://realpython.com/sorting-algorithms-python/
@@ -175,3 +195,114 @@ def quicksort(array):
     # The final result combines the sorted `low` list
     # with the `same` list and the sorted `high` list
     return quicksort(low) + same + quicksort(high)
+
+"""
+Timsort
+the complexity of Timsort is O(n log2n)
+Timsort performs exceptionally well on already-sorted or close-to-sorted lists,
+leading to a best-case scenario of O(n).
+In this case, Timsort clearly beats merge sort and matches the best-case scenario for Quicksort.
+But the worst case for Timsort is also O(n log2n), which surpasses Quicksort's O(n2).
+"""
+def insertion_sort(array, left=0, right=None):
+    if right is None:
+        right = len(array) - 1
+
+    # Loop from the element indicated by
+    # `left` until the element indicated by `right`
+    for i in range(left + 1, right + 1):
+        # This is the element we want to position in its
+        # correct place
+        key_item = array[i]
+
+        # Initialize the variable that will be used to
+        # find the correct position of the element referenced
+        # by `key_item`
+        j = i - 1
+
+        # Run through the list of items (the left
+        # portion of the array) and find the correct position
+        # of the element referenced by `key_item`. Do this only
+        # if the `key_item` is smaller than its adjacent values.
+        while j >= left and array[j] > key_item:
+            # Shift the value one position to the left
+            # and reposition `j` to point to the next element
+            # (from right to left)
+            array[j + 1] = array[j]
+            j -= 1
+
+        # When you finish shifting the elements, position
+        # the `key_item` in its correct location
+        array[j + 1] = key_item
+
+    return array
+
+def timsort(array):
+    min_run = 32
+    n = len(array)
+
+    # Start by slicing and sorting small portions of the
+    # input array. The size of these slices is defined by
+    # your `min_run` size.
+    for i in range(0, n, min_run):
+        insertion_sort(array, i, min((i + min_run - 1), n - 1))
+
+    # Now you can start merging the sorted slices.
+    # Start from `min_run`, doubling the size on
+    # each iteration until you surpass the length of
+    # the array.
+    size = min_run
+    while size < n:
+        # Determine the arrays that will
+        # be merged together
+        for start in range(0, n, size * 2):
+            # Compute the `midpoint` (where the first array ends
+            # and the second starts) and the `endpoint` (where
+            # the second array ends)
+            midpoint = start + size - 1
+            end = min((start + size * 2 - 1), (n-1))
+
+            # Merge the two subarrays.
+            # The `left` array should go from `start` to
+            # `midpoint + 1`, while the `right` array should
+            # go from `midpoint + 1` to `end + 1`.
+            merged_array = merge(
+                left=array[start:midpoint + 1],
+                right=array[midpoint + 1:end + 1])
+
+            # Finally, put the merged array back into
+            # your array
+            array[start:start + len(merged_array)] = merged_array
+
+        # Each iteration should double the size of your arrays
+        size *= 2
+
+    return array
+
+ARRAY_LENGTH = 10000
+
+if __name__ == "__main__":
+    # Generate an array of `ARRAY_LENGTH` items consisting
+    # of random integer values between 0 and 999
+
+    # array = [randint(0, 1000) for i in range(ARRAY_LENGTH)]
+    array = [randint(0, 1000) for i in range(ARRAY_LENGTH)]
+
+    # Call the function using the name of the sorting algorithm
+    # and the array you just created
+    run_sorting_algorithm(algorithm="sorted", array=array)
+    # Call each of the functions
+    run_sorting_algorithm(algorithm="bubble_sort", array=array)
+# Algorithm: bubble_sort. Minimum execution time: 73.21720498399998
+
+    run_sorting_algorithm(algorithm="insertion_sort", array=array)
+# Algorithm: insertion_sort. Minimum execution time: 56.71029764299999
+
+    run_sorting_algorithm(algorithm="merge_sort", array=array)
+# Algorithm: merge_sort. Minimum execution time: 0.6195857160000173
+
+    run_sorting_algorithm(algorithm="quicksort", array=array)
+# Algorithm: quicksort. Minimum execution time: 0.11675417600002902
+
+    run_sorting_algorithm(algorithm="timsort", array=array)
+# Algorithm: timsort. Minimum execution time: 0.5121690789999998
