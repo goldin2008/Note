675542894cc8d010c4659f04294e4c56d22b2c42
goldin2008 <goldin20082011@gmail.com>
2022-10-09 23:58:11 -0400

    Update

--- coding/basic_sorting.py
+++ coding/basic_sorting.py
@@ -172,6 +172,44 @@ def merge_sort(array):
         left=merge_sort(array[:midpoint]),
         right=merge_sort(array[midpoint:]))
 
+# Solution 2
+def mergeSort(array):
+    if len(array) > 1:
+
+        #  r is the point where the array is divided into two subarrays
+        r = len(array)//2
+        L = array[:r]
+        M = array[r:]
+
+        # Sort the two halves
+        mergeSort(L)
+        mergeSort(M)
+
+        i = j = k = 0
+
+        # Until we reach either end of either L or M, pick larger among
+        # elements L and M and place them in the correct position at A[p..r]
+        while i < len(L) and j < len(M):
+            if L[i] < M[j]:
+                array[k] = L[i]
+                i += 1
+            else:
+                array[k] = M[j]
+                j += 1
+            k += 1
+
+        # When we run out of elements in either L or M,
+        # pick up the remaining elements and put in A[p..r]
+        while i < len(L):
+            array[k] = L[i]
+            i += 1
+            k += 1
+
+        while j < len(M):
+            array[k] = M[j]
+            j += 1
+            k += 1
+
 """
 Quicksort
 Time Complexity Average: O(n*log n)
