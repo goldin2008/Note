abe7a972b2f362e9c6b0a266ea2cd7c3044bbe76
goldin2008 <goldin20082011@gmail.com>
2022-10-30 22:27:20 -0400

    Update

--- coding/meta/9_Palindrome Number.py
+++ coding/meta/9_Palindrome Number.py
@@ -5,3 +5,14 @@ An integer is a palindrome when it reads the same backward as forward.
 
 For example, 121 is a palindrome while 123 is not.
 """
+class Solution:
+    def isPalindrome(self, x: int) -> bool:
+        if x<0:
+            return False
+
+        inputNum = x
+        newNum = 0
+        while x>0:
+            newNum = newNum * 10 + x%10
+            x = x//10
+        return newNum == inputNum
