4332a843a762cce858521a2ed82856b9b74c47b4
goldin2008 <goldin20082011@gmail.com>
2022-10-30 22:29:38 -0400

    Update

--- coding/meta/1648_Sell Diminishing-Valued Colored Balls.py
+++ coding/meta/1648_Sell Diminishing-Valued Colored Balls.py
@@ -7,3 +7,16 @@ You are given an integer array, inventory, where inventory[i] represents the num
 
 Return the maximum total value that you can attain after selling orders colored balls. As the answer may be too large, return it modulo 109 + 7.
 """
+class Solution:
+    def maxProfit(self, inv: List[int], orders: int) -> int:
+        arr=sorted(Counter(inv).items(), reverse=True)+[(0,0)]
+        ans, ind, width=0,0,0
+        
+        while orders>0:
+            width += arr[ind][1]
+            sell=min(orders, width * (arr[ind][0] - arr[ind+1][0]))
+            whole, remainder= divmod(sell, width)
+            ans += width*(whole*(arr[ind][0]+arr[ind][0]-(whole-1)))//2 + remainder*(arr[ind][0]-whole)
+            orders -= sell
+            ind += 1
+        return ans % 1_000_000_007
