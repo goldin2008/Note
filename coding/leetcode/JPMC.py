c23fef3663063640b57463ca7e8e0725ac9e32ba
goldin2008 <goldin20082011@gmail.com>
2022-12-11 00:00:31 -0500

    Update

--- coding/leetcode/JPMC.py
+++ coding/leetcode/JPMC.py
@@ -1,4 +1,5 @@
 # 1. Array Reduction
+# https://www.geeksforgeeks.org/minimize-cost-to-reduce-the-array-to-a-single-element-by-given-operations/
 
 
 # 2. Break a Palindrome
@@ -17,9 +18,46 @@
 
 
 # 7. Highly Profitable Months
+# https://leetcode.com/discuss/interview-question/1400404/bridge-water-assoc-oa-number-of-strinctly-increasing-subarrays-of-size-k-in-an-array
 
 
 # 8. Subarray Sum
+# https://www.geeksforgeeks.org/sum-of-all-subarrays/
 
 
 # 9. Maximum Index
+
+
+# 10. 计算Fibonachi number，我用的带golden ratio的数学公式，时间复杂度是O(log n), 空间复杂度是O(1)
+
+
+# 11. Game Winner
+
+
+# 12. Cardinality Sorting (1356)
+# https://www.geeksforgeeks.org/sort-array-according-count-set-bits/
+
+# 13. Is Possible (780)
+
+
+# 14. Rearranging a Word (556)
+
+
+# 15. Lottery Coupons
+
+
+# 16. Self Descriptive Number
+# https://www.geeksforgeeks.org/self-descriptive-number/
+
+
+
+# 17. Find minimum number of coins that make a given value
+# https://www.geeksforgeeks.org/find-minimum-number-of-coins-that-make-a-change/
+# https://www.enjoyalgorithms.com/blog/minimum-coin-change
+
+
+
+# 18. Distinct Digit Numbers
+
+
+# 19. Balanced or Not
