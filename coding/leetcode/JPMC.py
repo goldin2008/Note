c8b7c5cf7ee7a8b16d5b393a211f1e9feb084535
goldin2008 <goldin20082011@gmail.com>
2022-12-10 23:33:26 -0500

    Update

---
+++ coding/leetcode/JPMC.py
@@ -0,0 +1,22 @@
+# Array Reduction
+
+
+# Break a Palindrome
+
+
+# Factors of 3 and 5
+
+
+# Selling Products
+
+
+# Longest Even Length Word
+
+
+# Missing Words
+
+
+# Highly Profitable Months
+
+
+# Subarray Sum
