6a380844994daeaf10dbb379773c83b415a5f5ae
goldin2008 <goldin20082011@gmail.com>
2022-12-23 00:52:53 -0500

    Update

--- coding/binarytree.py
+++ coding/binarytree.py
@@ -210,6 +210,36 @@ class Solution:
 
         return results
 
+"""
+二叉搜索树中的搜索
+"""
+# 递归法
+class Solution:
+    def searchBST(self, root: Optional[TreeNode], val: int) -> Optional[TreeNode]:
+        # 为什么要有返回值: 
+        #   因为搜索到目标节点就要立即return，
+        #   这样才是找到节点就返回（搜索某一条边），如果不加return，就是遍历整棵树了。
+        if not root or root.val == val: 
+            return root
+
+        if root.val > val: 
+            return self.searchBST(root.left, val)
+
+        if root.val < val: 
+            return self.searchBST(root.right, val)
+
+# 迭代法
+class Solution:
+    def searchBST(self, root: TreeNode, val: int) -> TreeNode:
+        while root is not None:
+            if val < root.val: root = root.left
+            elif val > root.val: root = root.right
+            else: return root
+        return None
+
+
+
+
 
 #1 226.翻转二叉树
 ## 递归法：前序遍历：
