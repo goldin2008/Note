d9af3f7513f916ca04a9ddcacc243ade2f418ae0
goldin2008 <goldin20082011@gmail.com>
2022-12-26 20:24:23 -0500

    Update

---
+++ coding/leetcode/0027_Remove Element.py
@@ -0,0 +1,13 @@
+class Solution:
+    def removeElement(self, nums: List[int], val: int) -> int:
+        # 快慢指针
+        fast = 0  # 快指针
+        slow = 0  # 慢指针
+        size = len(nums)
+        while fast < size:  # 不加等于是因为，a = size 时，nums[a] 会越界
+            # slow 用来收集不等于 val 的值，如果 fast 对应值不等于 val，则把它与 slow 替换
+            if nums[fast] != val:
+                nums[slow] = nums[fast]
+                slow += 1
+            fast += 1
+        return slow
