ae6799e714d216fb611cb34bef45792451903292
goldin2008 <goldin20082011@gmail.com>
2022-12-27 00:28:28 -0500

    Update

---
+++ coding/leetcode/0142_Linked List Cycle II.py
@@ -0,0 +1,17 @@
+class Solution:
+    def detectCycle(self, head: ListNode) -> ListNode:
+        slow, fast = head, head
+        while fast and fast.next:
+            slow = slow.next
+            fast = fast.next.next
+            # 如果相遇
+            if slow == fast:
+                p = head
+                q = slow
+                while p!=q:
+                    p = p.next
+                    q = q.next
+                #你也可以return q
+                return p
+
+        return None
