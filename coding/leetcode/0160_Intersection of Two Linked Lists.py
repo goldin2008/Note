b40f10866af7f9798670bd5a8eb3dd895f9d8978
goldin2008 <goldin20082011@gmail.com>
2022-12-27 00:25:33 -0500

    Update

---
+++ coding/leetcode/0160_Intersection of Two Linked Lists.py
@@ -0,0 +1,24 @@
+class Solution:
+    def getIntersectionNode(self, headA: ListNode, headB: ListNode) -> ListNode:
+        lenA, lenB = 0, 0
+        cur = headA
+        while cur:         # 求链表A的长度
+            cur = cur.next 
+            lenA += 1
+        cur = headB 
+        while cur:         # 求链表B的长度
+            cur = cur.next 
+            lenB += 1
+        curA, curB = headA, headB
+        if lenA > lenB:     # 让curB为最长链表的头，lenB为其长度
+            curA, curB = curB, curA
+            lenA, lenB = lenB, lenA 
+        for _ in range(lenB - lenA):  # 让curA和curB在同一起点上（末尾位置对齐）
+            curB = curB.next 
+        while curA:         #  遍历curA 和 curB，遇到相同则直接返回
+            if curA == curB:
+                return curA
+            else:
+                curA = curA.next 
+                curB = curB.next
+        return None 
