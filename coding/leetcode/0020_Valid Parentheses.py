6f61149fd38715dcccaacd7285d45a3f948cca0f
goldin2008 <goldin20082011@gmail.com>
2022-12-30 02:21:53 -0500

    Update

---
+++ coding/leetcode/0020_Valid Parentheses.py
@@ -0,0 +1,37 @@
+# 方法一，仅使用栈，更省空间
+class Solution:
+    def isValid(self, s: str) -> bool:
+        stack = []
+        
+        for item in s:
+            if item == '(':
+                stack.append(')')
+            elif item == '[':
+                stack.append(']')
+            elif item == '{':
+                stack.append('}')
+            elif not stack or stack[-1] != item:
+                return False
+            else:
+                stack.pop()
+        
+        return True if not stack else False
+
+
+# 方法二，使用字典
+class Solution:
+    def isValid(self, s: str) -> bool:
+        stack = []
+        mapping = {
+            '(': ')',
+            '[': ']',
+            '{': '}'
+        }
+        for item in s:
+            if item in mapping.keys():
+                stack.append(mapping[item])
+            elif not stack or stack[-1] != item: 
+                return False
+            else: 
+                stack.pop()
+        return True if not stack else False
