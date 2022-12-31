2ffc8f114e48584a0858c2d9a2a8cbe6c48183d3
goldin2008 <goldin20082011@gmail.com>
2022-12-31 15:02:23 -0500

    Update

--- coding/leetcode/0236_Lowest Common Ancestor of a Binary Tree.py
+++ coding/leetcode/0236_Lowest Common Ancestor of a Binary Tree.py
@@ -12,6 +12,7 @@
 如果搜索整个树，直接用一个变量left、right接住返回值，这个left、right后序还有逻辑处理的需要，
 也就是后序遍历中处理中间节点的逻辑（也是回溯）
 
+求最小公共祖先，需要从底向上遍历，那么二叉树，只能通过后序遍历（即:回溯）实现从底向上的遍历方式。
 """
 class Solution:
     def lowestCommonAncestor(self, root: 'TreeNode', p: 'TreeNode', q: 'TreeNode') -> 'TreeNode':
