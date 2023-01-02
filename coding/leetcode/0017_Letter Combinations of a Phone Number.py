d58365e9e791ecb796ef13a5d3acd1f94bc09bec
goldin2008 <goldin20082011@gmail.com>
2023-01-01 23:26:59 -0500

    Update

---
+++ coding/leetcode/0017_Letter Combinations of a Phone Number.py
@@ -0,0 +1,65 @@
+class Solution:
+    def __init__(self):
+        self.answers: List[str] = []
+        self.answer: str = ''
+        self.letter_map = {
+            '2': 'abc',
+            '3': 'def',
+            '4': 'ghi',
+            '5': 'jkl',
+            '6': 'mno',
+            '7': 'pqrs',
+            '8': 'tuv',
+            '9': 'wxyz'
+        }
+
+    def letterCombinations(self, digits: str) -> List[str]:
+        self.answers.clear()
+        if not digits: return []
+        self.backtracking(digits, 0)
+        return self.answers
+    
+    def backtracking(self, digits: str, index: int) -> None:
+        # 回溯函数没有返回值
+        # Base Case
+        if index == len(digits):    # 当遍历穷尽后的下一层时
+            self.answers.append(self.answer)
+            return 
+        # 单层递归逻辑  
+        letters: str = self.letter_map[digits[index]]
+        for letter in letters:
+            self.answer += letter   # 处理
+            self.backtracking(digits, index + 1)    # 递归至下一层
+            self.answer = self.answer[:-1]  # 回溯
+
+
+class Solution:
+    def __init__(self):
+        self.answers: List[str] = []
+        self.letter_map = {
+            '2': 'abc',
+            '3': 'def',
+            '4': 'ghi',
+            '5': 'jkl',
+            '6': 'mno',
+            '7': 'pqrs',
+            '8': 'tuv',
+            '9': 'wxyz'
+        }
+
+    def letterCombinations(self, digits: str) -> List[str]:
+        self.answers.clear()
+        if not digits: return []
+        self.backtracking(digits, 0, '')
+        return self.answers
+    
+    def backtracking(self, digits: str, index: int, answer: str) -> None:
+        # 回溯函数没有返回值
+        # Base Case
+        if index == len(digits):    # 当遍历穷尽后的下一层时
+            self.answers.append(answer)
+            return 
+        # 单层递归逻辑  
+        letters: str = self.letter_map[digits[index]]
+        for letter in letters:
+            self.backtracking(digits, index + 1, answer + letter)    # 递归至下一层 + 回溯
