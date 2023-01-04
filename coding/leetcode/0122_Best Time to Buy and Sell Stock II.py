8f54aa1ed60c9d37103fce224ffde9bca6cb08ea
goldin2008 <goldin20082011@gmail.com>
2023-01-03 22:42:38 -0500

    Update

---
+++ coding/leetcode/0122_Best Time to Buy and Sell Stock II.py
@@ -0,0 +1,20 @@
+# 贪心
+class Solution:
+    def maxProfit(self, prices: List[int]) -> int:
+        result = 0
+        for i in range(1, len(prices)):
+            result += max(prices[i] - prices[i - 1], 0)
+        return result
+
+
+# 动态规划
+class Solution:
+    def maxProfit(self, prices: List[int]) -> int:
+        length = len(prices)
+        dp = [[0] * 2 for _ in range(length)]
+        dp[0][0] = -prices[0]
+        dp[0][1] = 0
+        for i in range(1, length):
+            dp[i][0] = max(dp[i-1][0], dp[i-1][1] - prices[i]) #注意这里是和121. 买卖股票的最佳时机唯一不同的地方
+            dp[i][1] = max(dp[i-1][1], dp[i-1][0] + prices[i])
+        return dp[-1][1]
