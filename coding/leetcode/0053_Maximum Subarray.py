45de9d0147b6caf6ba070e2850c63d51ffd7f9b6
goldin2008 <goldin20082011@gmail.com>
2023-01-03 22:40:05 -0500

    Update

---
+++ coding/leetcode/0053_Maximum Subarray.py
@@ -0,0 +1,11 @@
+class Solution:
+    def maxSubArray(self, nums: List[int]) -> int:
+        result = -float('inf')
+        count = 0
+        for i in range(len(nums)):
+            count += nums[i]
+            if count > result:
+                result = count
+            if count <= 0:
+                count = 0
+        return result
