75dca8f3e0064c90e23eb98d9ce9fa6bd2c38574
goldin2008 <goldin20082011@gmail.com>
2023-01-05 00:09:34 -0500

    Update

---
+++ coding/leetcode/0062_Unique Paths.py
@@ -0,0 +1,7 @@
+class Solution: # 动态规划
+    def uniquePaths(self, m: int, n: int) -> int:
+        dp = [[1 for i in range(n)] for j in range(m)]
+        for i in range(1, m):
+            for j in range(1, n):
+                dp[i][j] = dp[i][j - 1] + dp[i - 1][j]
+        return dp[m - 1][n - 1]
