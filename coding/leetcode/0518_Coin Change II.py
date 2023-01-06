4aa401db8e8617bbb2efbbffa8071bd676c4460d
goldin2008 <goldin20082011@gmail.com>
2023-01-06 04:51:45 -0500

    Update

---
+++ coding/leetcode/0518_Coin Change II.py
@@ -0,0 +1,10 @@
+class Solution:
+    def change(self, amount: int, coins: List[int]) -> int:
+        dp = [0]*(amount + 1)
+        dp[0] = 1
+        # 遍历物品
+        for i in range(len(coins)):
+            # 遍历背包
+            for j in range(coins[i], amount + 1):
+                dp[j] += dp[j - coins[i]]
+        return dp[amount]
