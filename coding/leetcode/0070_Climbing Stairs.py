54b3c68542b16648b137cadc7b70ba1c132f295f
goldin2008 <goldin20082011@gmail.com>
2023-01-07 00:07:46 -0500

    Update

--- coding/leetcode/0070_Climbing Stairs.py
+++ coding/leetcode/0070_Climbing Stairs.py
@@ -20,3 +20,16 @@ class Solution:
             dp[0] = dp[1]
             dp[1] = tmp
         return dp[1]
+
+class Solution:
+    def climbStairs(self, n: int) -> int:
+        dp = [0]*(n + 1)
+        dp[0] = 1
+        m = 2
+        # 遍历背包
+        for j in range(n + 1):
+            # 遍历物品
+            for step in range(1, m + 1):
+                if j >= step:
+                    dp[j] += dp[j - step]
+        return dp[n]
