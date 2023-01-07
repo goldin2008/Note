418be56b107bb7e47c37d1e3710e3621622e2f57
goldin2008 <goldin20082011@gmail.com>
2023-01-07 00:06:29 -0500

    Update

---
+++ coding/leetcode/0377_Combination Sum IV.py
@@ -0,0 +1,11 @@
+class Solution:
+    def combinationSum4(self, nums, target):
+        dp = [0] * (target + 1)
+        dp[0] = 1
+
+        for i in range(1, target+1):
+            for j in nums:
+                if i >= j:
+                    dp[i] += dp[i - j]
+
+        return dp[-1]
