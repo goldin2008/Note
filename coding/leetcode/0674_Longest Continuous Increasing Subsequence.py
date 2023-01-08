8d7053e6a9fd2ba47c66515f0d0dcf74a53c725b
goldin2008 <goldin20082011@gmail.com>
2023-01-08 00:27:43 -0500

    Update

---
+++ coding/leetcode/0674_Longest Continuous Increasing Subsequence.py
@@ -0,0 +1,27 @@
+# 动态规划：
+class Solution:
+    def findLengthOfLCIS(self, nums: List[int]) -> int:
+        if len(nums) == 0:
+            return 0
+        result = 1
+        dp = [1] * len(nums)
+        for i in range(len(nums)-1):
+            if nums[i+1] > nums[i]: #连续记录
+                dp[i+1] = dp[i] + 1
+            result = max(result, dp[i+1])
+        return result
+
+# 贪心法：
+class Solution:
+    def findLengthOfLCIS(self, nums: List[int]) -> int:
+        if len(nums) == 0:
+            return 0
+        result = 1 #连续子序列最少也是1
+        count = 1
+        for i in range(len(nums)-1):
+            if nums[i+1] > nums[i]: #连续记录
+                count += 1
+            else: #不连续，count从头开始
+                count = 1
+            result = max(result, count)
+        return result
