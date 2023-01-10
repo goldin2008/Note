d38fab5dd2e68e9c0ded6ed4595bc36c23b8529c
goldin2008 <goldin20082011@gmail.com>
2023-01-10 14:01:51 -0500

    Update

---
+++ coding/leetcode/0189_Rotate Array.py
@@ -0,0 +1,12 @@
+class Solution:
+    def rotate(self, A: List[int], k: int) -> None:
+        def reverse(i, j):
+            while i < j:
+                A[i], A[j] = A[j], A[i]
+                i += 1
+                j -= 1
+        n = len(A)
+        k %= n
+        reverse(0, n - 1)
+        reverse(0, k - 1)
+        reverse(k, n - 1)
