28624684b8ef19f8b9880446d04e90e2fd64c780
goldin2008 <goldin20082011@gmail.com>
2023-01-11 00:12:33 -0500

    Update

---
+++ coding/leetcode/0141_Linked List Cycle.py
@@ -0,0 +1,10 @@
+class Solution:
+    def hasCycle(self, head: ListNode) -> bool:
+        if not head: return False
+        slow, fast = head, head
+        while fast and fast.next:
+            slow = slow.next
+            fast = fast.next.next
+            if fast == slow:
+                return True
+        return False
