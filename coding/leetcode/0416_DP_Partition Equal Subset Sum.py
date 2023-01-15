4044447d9b16b0b14b3e73eae09fac61d403cc87
goldin2008 <goldin20082011@gmail.com>
2023-01-15 13:55:11 -0500

    Update

--- coding/leetcode/0416_DP_Partition Equal Subset Sum.py
+++ coding/leetcode/0416_DP_Partition Equal Subset Sum.py
@@ -8,6 +8,15 @@ dp[i][j] = max(dp[i][j], dp[i][j - weight[i]] + value[i]);
 这就是滚动数组的由来，需要满足的条件是上一层可以重复利用，直接拷贝到当前层。
 读到这里估计大家都忘了 dp[i][j]里的i和j表达的是什么了，i是物品，j是背包容量。
 dp[i][j] 表示从下标为[0-i]的物品里任意取，放进容量为j的背包，价值总和最大是多少
+
+dp[j]可以通过dp[j - weight[i]]推导出来，dp[j - weight[i]]表示容量为j - weight[i]的背包所背的最大价值。
+
+dp[j - weight[i]] + value[i] 表示 容量为 j - 物品i重量 的背包 加上 物品i的价值。
+也就是容量为j的背包，放入物品i了之后的价值即:dp[j]）
+
+此时dp[j]有两个选择，一个是取自己dp[j] 相当于 二维dp数组中的dp[i-1][j]，即不放物品i
+一个是取dp[j - weight[i]] + value[i]，即放物品i，指定是取最大的，毕竟是求最大价值，
+dp[j] = max(dp[j], dp[j - weight[i]] + value[i]);
 """
 
 class Solution:
