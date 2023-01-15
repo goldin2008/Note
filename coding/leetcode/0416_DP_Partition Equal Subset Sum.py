23c4f4a1bd90d55c0fc1ed96b722d43e4763e766
goldin2008 <goldin20082011@gmail.com>
2023-01-15 13:52:33 -0500

    Update

--- coding/leetcode/0416_DP_Partition Equal Subset Sum.py
+++ coding/leetcode/0416_DP_Partition Equal Subset Sum.py
@@ -1,3 +1,15 @@
+"""
+对于背包问题其实状态都是可以压缩的。
+在使用二维数组的时候，递推公式: 
+dp[i][j] = max(dp[i - 1][j], dp[i - 1][j - weight[i]] + value[i]);
+其实可以发现如果把dp[i - 1]那一层拷贝到dp[i]上，表达式完全可以是: 
+dp[i][j] = max(dp[i][j], dp[i][j - weight[i]] + value[i]);
+与其把dp[i - 1]这一层拷贝到dp[i]上，不如只用一个一维数组了，只用dp[j]（一维数组，也可以理解是一个滚动数组）。
+这就是滚动数组的由来，需要满足的条件是上一层可以重复利用，直接拷贝到当前层。
+读到这里估计大家都忘了 dp[i][j]里的i和j表达的是什么了，i是物品，j是背包容量。
+dp[i][j] 表示从下标为[0-i]的物品里任意取，放进容量为j的背包，价值总和最大是多少
+"""
+
 class Solution:
     def canPartition(self, nums: List[int]) -> bool:
         target = sum(nums)
