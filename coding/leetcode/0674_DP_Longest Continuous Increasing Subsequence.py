92448e39f56bffbcd7d889f34f5b352ae1170477
goldin2008 <goldin20082011@gmail.com>
2023-01-21 21:30:40 -0500

    Update

--- coding/leetcode/0674_DP_Longest Continuous Increasing Subsequence.py
+++ coding/leetcode/0674_DP_Longest Continuous Increasing Subsequence.py
@@ -1,3 +1,9 @@
+"""
+dp[i]: 以下标i为结尾的连续递增的子序列长度为dp[i]。
+注意这里的定义，一定是以下标i为结尾，并不是说一定以下标0为起始位置。
+
+贪心法更好理解，而且空间复杂度为O(1)，比动规O(n)更好
+"""
 # 动态规划：
 class Solution:
     def findLengthOfLCIS(self, nums: List[int]) -> int:
