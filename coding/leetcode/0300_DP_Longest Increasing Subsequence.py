aa1db07b16fedafe04935e029d4158ff4ef84a79
goldin2008 <goldin20082011@gmail.com>
2023-01-21 21:15:53 -0500

    Update

--- coding/leetcode/0300_DP_Longest Increasing Subsequence.py
+++ coding/leetcode/0300_DP_Longest Increasing Subsequence.py
@@ -1,3 +1,10 @@
+"""
+dp[i] = max(dp[i], dp[j] + 1)
+位置i的最长升序子序列等于j从0到i-1各个位置的最长升序子序列 + 1 的最大值。
+dp[i] 是有0到i-1各个位置的最长递增子序列 推导而来，那么遍历i一定是从前向后遍历。
+
+现用dp[i]记录内层循环最大值，再用result记录外层循环，也是整个的最大值.
+"""
 class Solution:
     def lengthOfLIS(self, nums: List[int]) -> int:
         if len(nums) <= 1:
