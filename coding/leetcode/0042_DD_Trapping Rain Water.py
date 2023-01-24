704dde3ec0a26090015bff76150cb659ef42a3b6
goldin2008 <goldin20082011@gmail.com>
2023-01-24 00:03:38 -0500

    Update

--- coding/leetcode/0042_DD_Trapping Rain Water.py
+++ coding/leetcode/0042_DD_Trapping Rain Water.py
@@ -1,50 +1,11 @@
-# 双指针法
-class Solution:
-    def trap(self, height: List[int]) -> int:
-        res = 0
-        for i in range(len(height)):
-            if i == 0 or i == len(height)-1: continue
-            lHight = height[i-1]
-            rHight = height[i+1]
-            for j in range(i-1):
-                if height[j] > lHight:
-                    lHight = height[j]
-            for k in range(i+2,len(height)):
-                if height[k] > rHight:
-                    rHight = height[k]
-            res1 = min(lHight,rHight) - height[i]        
-            if res1 > 0:
-                res += res1
-        return res
-
-
-# 动态规划
-class Solution:
-    def trap(self, height: List[int]) -> int:
-        leftheight, rightheight = [0]*len(height), [0]*len(height)
-        
-        leftheight[0]=height[0]
-        for i in range(1,len(height)):
-            leftheight[i]=max(leftheight[i-1],height[i])
-        rightheight[-1]=height[-1]
-        for i in range(len(height)-2,-1,-1):
-            rightheight[i]=max(rightheight[i+1],height[i])
-        
-        result = 0
-        for i in range(0,len(height)):
-            summ = min(leftheight[i],rightheight[i])-height[i]
-            result += summ
-        return result
-
-
 # 单调栈
 class Solution:
     def trap(self, height: List[int]) -> int:
         # 单调栈
         '''
         单调栈是按照 行 的方向来计算雨水
-        从栈顶到栈底的顺序：从小到大
-        通过三个元素来接水：栈顶，栈顶的下一个元素，以及即将入栈的元素
+        从栈顶到栈底的顺序: 从小到大
+        通过三个元素来接水: 栈顶，栈顶的下一个元素，以及即将入栈的元素
         雨水高度是 min(凹槽左边高度, 凹槽右边高度) - 凹槽底部高度
         雨水的宽度是 凹槽右边的下标 - 凹槽左边的下标 - 1（因为只求中间宽度）
         '''
@@ -98,4 +59,42 @@ class Solution:
                     # 累计总雨水体积
                     result += h * w
             stack.append(i)
+        return result
+
+# 双指针法
+class Solution:
+    def trap(self, height: List[int]) -> int:
+        res = 0
+        for i in range(len(height)):
+            if i == 0 or i == len(height)-1: continue
+            lHight = height[i-1]
+            rHight = height[i+1]
+            for j in range(i-1):
+                if height[j] > lHight:
+                    lHight = height[j]
+            for k in range(i+2,len(height)):
+                if height[k] > rHight:
+                    rHight = height[k]
+            res1 = min(lHight,rHight) - height[i]        
+            if res1 > 0:
+                res += res1
+        return res
+
+
+# 动态规划
+class Solution:
+    def trap(self, height: List[int]) -> int:
+        leftheight, rightheight = [0]*len(height), [0]*len(height)
+        
+        leftheight[0]=height[0]
+        for i in range(1,len(height)):
+            leftheight[i]=max(leftheight[i-1],height[i])
+        rightheight[-1]=height[-1]
+        for i in range(len(height)-2,-1,-1):
+            rightheight[i]=max(rightheight[i+1],height[i])
+        
+        result = 0
+        for i in range(0,len(height)):
+            summ = min(leftheight[i],rightheight[i])-height[i]
+            result += summ
         return result
