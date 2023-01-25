007b3b7183f24e066a8b5519025b03c715b617cb
goldin2008 <goldin20082011@gmail.com>
2023-01-25 18:04:41 -0500

    Update

--- coding/leetcode/0283_EX_Move Zeroes.py
+++ coding/leetcode/0283_EX_Move Zeroes.py
@@ -1,3 +1,13 @@
+# 交换前后变量，避免补零
+class Solution:
+    def moveZeroes(self, nums: List[int]) -> None:
+        slow, fast = 0, 0
+        while fast < len(nums):
+            if nums[fast] != 0:
+                nums[slow], nums[fast] = nums[fast], nums[slow]
+                slow += 1 # 保持[0, slow)区间是没有0的
+            fast += 1
+
 class Solution:
     def moveZeroes(self, nums: List[int]) -> None:
         """
@@ -10,12 +20,3 @@ class Solution:
                 slow += 1
         for i in range(slow, len(nums)):
             nums[i] = 0
-
-# 交换前后变量，避免补零
-    def moveZeroes(self, nums: List[int]) -> None:
-        slow, fast = 0, 0
-        while fast < len(nums):
-            if nums[fast] != 0:
-                nums[slow], nums[fast] = nums[fast], nums[slow]
-                slow += 1 # 保持[0, slow)区间是没有0的
-            fast += 1
