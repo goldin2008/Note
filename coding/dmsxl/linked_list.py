d884e55378e97da8775fc77ed527e986c5b69e57
goldin2008 <goldin20082011@gmail.com>
2023-02-02 13:36:08 -0500

    Update

--- coding/dmsxl/linked_list.py
+++ coding/dmsxl/linked_list.py
@@ -1,4 +1,18 @@
 """
-
+设置一个虚拟头结点在进行删除操作
 """
-
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
+class Solution:
+    def removeElements(self, head: ListNode, val: int) -> ListNode:
+        dummy_head = ListNode(next=head) #添加一个虚拟节点
+        cur = dummy_head
+        while(cur.next!=None):
+            if(cur.next.val == val):
+                cur.next = cur.next.next #删除cur.next节点
+            else:
+                cur = cur.next
+        return dummy_head.next
