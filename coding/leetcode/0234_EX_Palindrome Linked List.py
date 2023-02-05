9a47d35a91ab51f25e885f9e79182d92a9c15e0a
goldin2008 <goldin20082011@gmail.com>
2023-02-05 18:01:25 -0500

    Update

--- coding/leetcode/0234_EX_Palindrome Linked List.py
+++ coding/leetcode/0234_EX_Palindrome Linked List.py
@@ -1,3 +1,43 @@
+ #反转后半部分链表
+class Solution:
+    def isPalindrome(self, head: Optional[ListNode]) -> bool:
+
+        # Find the end of first half and reverse second half.
+
+        # find mid point which including (first) mid point into the first half linked list
+        first_half_end = self.end_of_first_half(head)
+        # reverse second half linked list
+        second_half_start = self.reverse_list(first_half_end.next)
+
+        # compare reversed and original half; must maintain reversed linked list is shorter than 1st half
+        left = head
+        right = second_half_start
+        while right:
+            if right.val != left.val:
+                return False
+            right = right.next
+            left = left.next
+        return True
+
+    def end_of_first_half(self, head):
+        fast = head
+        slow = head
+        while fast.next and fast.next.next:
+            fast = fast.next.next
+            slow = slow.next
+        return slow
+
+    def reverse_list(self, head):
+        pre = None # dummy head
+        cur = head
+        while cur:
+            temp = cur.next
+            cur.next = pre
+            pre = cur
+            cur = temp
+        return pre
+
+
 #数组模拟
 class Solution:
     def isPalindrome(self, head: Optional[ListNode]) -> bool:
@@ -11,27 +51,4 @@ class Solution:
                 return False
             l+=1
             r-=1
-        return True   
-      
-#反转后半部分链表
-class Solution:
-    def isPalindrome(self, head: Optional[ListNode]) -> bool:
-        fast = slow = head
-
-        # find mid point which including (first) mid point into the first half linked list
-        while fast and fast.next:
-            fast = fast.next.next
-            slow = slow.next
-        node = None
-
-        # reverse second half linked list
-        while slow:
-            slow.next, slow, node = node, slow.next, slow
-
-        # compare reversed and original half; must maintain reversed linked list is shorter than 1st half
-        while node:
-            if node.val != head.val:
-                return False
-            node = node.next
-            head = head.next
         return True
