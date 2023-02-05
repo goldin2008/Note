f9babb38eb0fef95b3ca00c84b5c76fd542516ef
goldin2008 <goldin20082011@gmail.com>
2023-02-05 18:22:59 -0500

    Update

--- coding/leetcode/0234_EX_Palindrome Linked List.py
+++ coding/leetcode/0234_EX_Palindrome Linked List.py
@@ -1,13 +1,18 @@
- #反转后半部分链表
+#反转后半部分链表
 class Solution:
     def isPalindrome(self, head: Optional[ListNode]) -> bool:
 
         # Find the end of first half and reverse second half.
 
+        # Method 1
         # find mid point which including (first) mid point into the first half linked list
         first_half_end = self.end_of_first_half(head)
         # reverse second half linked list
         second_half_start = self.reverse_list(first_half_end.next)
+
+        # Method 2
+        # first_half_end = self.end_of_first_half_2(head)
+        # second_half_start = self.reverse_list(first_half_end)
 
         # compare reversed and original half; must maintain reversed linked list is shorter than 1st half
         left = head
@@ -23,6 +28,14 @@ class Solution:
         fast = head
         slow = head
         while fast.next and fast.next.next:
+            fast = fast.next.next
+            slow = slow.next
+        return slow
+
+    def end_of_first_half_2(self, head):
+        fast = head
+        slow = head
+        while fast and fast.next:
             fast = fast.next.next
             slow = slow.next
         return slow
